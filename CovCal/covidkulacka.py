c3d90572c132a074db71b4e9c890087da17a1599
Lacin0o <92881138+Lacin0o@users.noreply.github.com>
2021-10-21 15:31:21 +0200

    Update covidkulacka.py

--- CovCal/covidkulacka.py
+++ CovCal/covidkulacka.py
@@ -8,41 +8,41 @@ ppl = "ludi,"
 u2 = "dufam ze si medzi nimi aj ty."
 
 print("\n")
-print(welcome, "\n" )
+print(welcome, "\n")
 
 while True:
-    
-    x = float(input("Kolko je nakazenych dnes?: " ))
-    
+
+    x = float(input("Kolko je nakazenych dnes?: "))
+
     if x == 69:
-        print("\n" "Nice" "\n" )
-    
+        print("\n" "Nice" "\n")
+
     if x >= 1000:
-        print("\n" "Pokazili sme si to" "\n" )
-    
+        print("\n" "Pokazili sme si to" "\n")
+
     y = int(input("Pocet dni predikcie sirenia od dnes: "))
-    
+
     if y == 22:
-        print("\n" "...uz len 22 dni, prosim nezabudni...""\n")
-        
+        print("\n" "...uz len 22 dni, prosim nezabudni..." "\n")
+
     if x == 9 and y == 11:
         print("\n" "9/11 Was an Inside Job")
-    
-    z = float(x*(1.013**y))
-    f = float(z*0.097045)
-    
-    f_round = round(f,2)
-    z_round = round(z,2)
-    
-    print("\n",count, y, day, z_round,"\n")
-    print(dead, f_round, ppl, u2,"\n")
-    
+
+    z = float(x * (1.013 ** y))
+    f = float(z * 0.097045)
+
+    f_round = round(f, 2)
+    z_round = round(z, 2)
+
+    print("\n", count, y, day, z_round, "\n")
+    print(dead, f_round, ppl, u2, "\n")
+
     while True:
         again = str(input("Spustit znovu? (y/n): "))
         if again in ("y", "n"):
             break
-    
-    if again == 'y':
+
+    if again == "y":
         continue
 
     if again == "n":
