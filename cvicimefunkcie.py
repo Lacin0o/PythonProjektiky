88317f462da6203aa501f1b53fd5f58e751ac4cb
Lacin0o <92881138+Lacin0o@users.noreply.github.com>
2021-10-24 15:02:57 +0200

    Create cvicimefunkcie.py

---
+++ cvicimefunkcie.py
@@ -0,0 +1,48 @@
+from time import sleep
+from typing import Text
+
+
+
+while True: 
+    
+    x = int(input("Zadaj prve cislo: "))
+    y = int(input("Zadaj druhe cislo: "))
+    op = str(input("Vyber operator(+,-,*,/): "))
+
+
+
+    
+    def plus():
+        z = x + y
+        print("vysledok je: ", z)
+        
+    def minus():
+        z = x - y
+        print("vysledok je: ", z)
+
+    def krat():
+        z = x * y
+        print("Vysledok je: ", z)
+
+    def delene():
+        z = x / y
+        print("Vysledok je: ", z)
+
+
+
+    if op == "+":
+        plus()
+
+    if op =="-":
+        minus()
+
+    if op =="*":
+        krat()
+
+    if op =="/":
+        delene()
+
+    while True:
+        again = str(input("Spustit znovu? (y/n): "))
+        if again in ("y", "n"):
+            break
