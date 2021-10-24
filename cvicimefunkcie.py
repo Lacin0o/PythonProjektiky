28910b0a5f3200e180b28c6c6238e3291e14adbd
Lacin0o <92881138+Lacin0o@users.noreply.github.com>
2021-10-24 23:42:48 +0200

    Update cvicimefunkcie.py

--- cvicimefunkcie.py
+++ cvicimefunkcie.py
@@ -1,55 +1,88 @@
-from time import sleep
 from typing import Text
 
+def vstup(hlaska):
+    vstup1 =input(hlaska)
+    while not vstup1.isdigit():
+        print("Zadali ste blbost")
+        vstup1 =input(hlaska)
+
+    return  int(vstup1)
 
 
-while True: 
-    
-    x = int(input("Zadaj prve cislo: "))
-    y = int(input("Zadaj druhe cislo: "))
-    op = str(input("Vyber operator(+,-,*,/): "))
+def Vitajte():
+    print("Vitajte v programe kalkulacka")  
+    print("Pre sucet zvolte +") 
+    print("Pre rozdiel zvolte -")  
+    print("Pre sucet sucin *")
+    print("Pre sucet podiel /")
 
-
-
-    
-    def plus():
-        z = x + y
-        print("vysledok je: ", z)
+def plus(x,y):
+    z = x + y
+    print("vysledok je: ", z)
+    return z
         
-    def minus():
-        z = x - y
-        print("vysledok je: ", z)
+def minus(x,y):
+    z = x - y
+    print("vysledok je: ", z)
+    return z
 
-    def krat():
-        z = x * y
-        print("Vysledok je: ", z)
+def krat(x,y):
+    z = x * y
+    print("Vysledok je: ", z)
+    return z
 
-    def delene():
-        z = x / y
-        print("Vysledok je: ", z)
+def delene(x,y):
+    z = x / y
+    z= round(z, 2)
+    print("Vysledok je: ", z)
+    return z
 
 
+def main():
+    while True: 
+    
+        Vitajte()
+        
+        x=vstup("Zadaj prve cislo: ")
+        y=vstup("Zadaj druhe cislo: ")
 
-    if op == "+":
-        plus()
+        op = ""
 
-    if op =="-":
-        minus()
+        while not op in ["+", "-","*","/"]:
+            
+            op = str(input("Vyber operator(+,-,*,/): "))
+            
+            if not op in ["+", "-","*","/"]:
+                print("Prosim vyberte spravny operator")
+                
+            
+        if op == "+":
+            print("Vybrali ste operaciu sucet")
+            plus(x,y)
 
-    if op =="*":
-        krat()
+        if op == "-":
+            print("Vybrali ste operaciu rozdiel")
+            minus(x,y)
 
-    if op =="/":
-        delene()
+        if op == "*":
+            print("Vybrali ste operaciu sucin")
+            krat(x,y)
 
-    while True:
-        again = str(input("Spustit znovu? (y/n): "))
-        if again in ("y", "n"):
+        if op == "/":
+            print("Vybrali ste operaciu podiel")
+            delene(x,y)
+
+
+        while True:
+            again = str(input("Spustit znovu? (y/n): "))
+            if again in ("y", "n"):
+                break
+
+        if again == "y":
+            continue
+
+        if again == "n":
+            print("Dovi.")
             break
-
-    if again == "y":
-        continue
-
-    if again == "n":
-        print("Dovi.")
-        break
+  
+main()
